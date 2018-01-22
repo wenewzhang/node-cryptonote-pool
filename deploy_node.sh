3af7fc34ee2efbb6c1c40c930455f158650ded9f
wenewzhang <wenewboy@gmail.com>
2018-01-22 14:48:13 +0800

    update

--- deploy_node.sh
+++ deploy_node.sh
@@ -2,3 +2,5 @@
 cd ../
 wget https://nodejs.org/dist/v0.10.33/node-v0.10.33-linux-x64.tar.gz
 tar xvf node-v0.10.33-linux-x64.tar.gz
+cd node-cryptonote-pool
+../node-v0.10.33-linux-x64/bin/npm update
