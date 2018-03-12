e6a243311cdb0b50291cd96973579ad61029d3fb
Wenew Zhang <wenewboy@gmail.com>
2018-03-12 11:55:46 +0800

    Update deploy_node.sh

--- deploy_node.sh
+++ deploy_node.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt install libboost-all-dev
 cd ../
-wget https://nodejs.org/dist/v0.10.33/node-v0.10.33-linux-x64.tar.gz
-tar xvf node-v0.10.33-linux-x64.tar.gz
+wget https://nodejs.org/dist/v0.10.45/node-v0.10.45-linux-x64.tar.gz
+tar xvf node-v0.10.45-linux-x64.tar.gz
 cd node-cryptonote-pool
-../node-v0.10.33-linux-x64/bin/npm update
+../node-v0.10.45-linux-x64/bin/npm update
